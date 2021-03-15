8ce1631dd1d1fe1e0828ef1ad79d7f81593c99a0
lizcramerfox <ecfox2008@gmail.com>
2021-03-15 13:38:34 -0400

    Adds routes

--- app/server.js
+++ app/server.js
@@ -21,13 +21,15 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to bezkoder application.'})
 })
 
+require('./app/routes/tutorial.routes')(app)
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
 
-const db = require('./app/models')
+const db = require('./models')
 db.sequelize.sync({ force: true }).then(() => {
   console.log('Drop and re-sync db.')
 })
